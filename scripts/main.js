f2c3baa1b19c4a03957d203159ac365866064045
lspangenberg <laurens.am.spangenberg@gmail.com>
2022-03-11 21:15:44 +0100

    Add functionality to create new todos

--- scripts/main.js
+++ scripts/main.js
@@ -18,8 +18,6 @@ function updateCounters() {
 
 }
 
-updateCounters();
-
 function toggleDone(event) {
 
   if (this.hasAttribute('checked')) {
@@ -33,8 +31,56 @@ function toggleDone(event) {
   updateCounters();
 }
 
+//Attach checkboxes to events to toggle its state
 const toDoItems = toDoList.querySelectorAll('li input');
 
 for (var i = toDoItems.length - 1; i >= 0; i--) {
   toDoItems[i].addEventListener('change', toggleDone);
 };
+
+function createItem(title) {
+  // create a label
+
+  console.log('Added new item:', title);
+
+  const itemNum = "todoItem" + parseInt(toDoList.children.length + 1);
+  const newLabel = document.createElement('label');
+  const newCheckbox = document.createElement('input');
+  const newLi = document.createElement('li');
+
+  //create label
+  newLabel.setAttribute('for', itemNum);
+  newLabel.innerText = title;
+
+  //create checkbox
+  newCheckbox.setAttribute('type', 'checkbox');
+  newCheckbox.setAttribute('id', itemNum);
+  newCheckbox.setAttribute('name', 'toDoItems');
+  newCheckbox.addEventListener('change', toggleDone);
+
+  //append to a new <li> and then to the list
+  newLi.appendChild(newCheckbox);
+  newLi.appendChild(newLabel);
+  toDoList.appendChild(newLi);
+}
+
+
+document
+  .querySelector("form")
+  .addEventListener("submit", function addNewItem(event){
+    event.preventDefault();
+
+    const inputField = document.querySelector('input[type="text"]');
+    const newItemTitle = inputField.value;
+    createItem(newItemTitle);
+
+    // reset the value of the inputField to make it empty and
+    // ready to create new todos
+    inputField.value = null;
+
+    updateCounters();
+  });
+
+
+//Update the counters once upon page load
+updateCounters();
